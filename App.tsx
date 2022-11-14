4be1baf419b681d8ba3db762cae77555e99d81ed
dim145 <dimitri.bleach@gmail.com>
2022-11-14 12:43:56 +0100

    add base management

--- App.tsx
+++ App.tsx
@@ -1,45 +1,108 @@
 import { StatusBar } from 'expo-status-bar';
-import {Button, FlatList, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {
+  Button,
+  DatePickerAndroid,
+  DatePickerIOS,
+  FlatList,
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View
+} from 'react-native';
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import {useState} from "react";
 import {NavigationContainer} from "@react-navigation/native";
+import DateTimePicker, {DateTimePickerAndroid} from '@react-native-community/datetimepicker';
+import RNDateTimePicker from "@react-native-community/datetimepicker";
 
 type ToDo = {
   title: string,
   text: string,
-  date: Date
+  date: Date,
+  state: boolean
 };
 
 const baseData: ToDo[] = [
   {
     title: 'Jouer à pokemon écarlate',
     text: 'Attraper les 4 légendaires',
-    date: new Date()
+    date: new Date(),
+    state: false
   },
   {
     title: 'Jouer à God of War (R)',
     text: 'Niker Thor',
-    date: new Date()
+    date: new Date(),
+    state: false
+  },
+  {
+    title: 'Jouer à mario kart',
+    text: 'Terminer le jeu à 100%',
+    date: new Date(),
+    state: true
   }
 ]
 
 const Stack = createNativeStackNavigator();
 
-function ToDoListScreen({navigation}: any)
+function SqlAdd(item: ToDo)
+{
+  console.log('sql add', item);
+}
+
+function ToDoListScreen({route, navigation}: any)
 {
    const [todolist, setToDoList] = useState<ToDo[]>(baseData);
 
-   const navigate = (title: string) => navigation.navigate('ToDo', {
-     title: title,
+    const newItem = (route.params || {}).newItem;
+
+    if(newItem)
+    {
+      const items = [...todolist];
+
+      items.push(newItem);
+
+      setToDoList(items);
+      SqlAdd(newItem);
+      route.params.newItem = undefined;
+    }
+
+   const pending = todolist.filter(f => !f.state);
+   const done = todolist.filter(f => f.state);
+
+   const onLongPressItem = (item: ToDo) => {
+     item.state = !item.state;
+
+     setToDoList([...todolist]);
+   };
+
+   const onPressItem = (item: ToDo) => navigation.navigate('ToDo', {
+     title: item.title,
      todolist: todolist
    });
 
    return <SafeAreaView>
      <View>
-       <Button title='Add Button' color={'blue'} />
+       <Button title='Add Todo' color={'blue'} onPress={() => navigation.navigate('AddItem')} />
+       <Button title='Remove Todos' onPress={() => setToDoList([])} />
 
-       <FlatList data={todolist} renderItem={({item}) => <View>
-         <Text onPress={() => navigate(item.title)}>{item.title}</Text>
+       <Text style={styles.listTitle}>Pending</Text>
+       <FlatList data={pending} renderItem={({item}) => <View>
+         <Text
+             onPress={() => onPressItem(item)}
+            onLongPress={() => onLongPressItem(item)}
+         >{item.title}</Text>
+       </View>} />
+
+       <Text style={styles.listTitle}>Done</Text>
+       <FlatList data={done} renderItem={({item}) => <View>
+         <Text
+             onPress={() => onPressItem(item)}
+         onLongPress={() => onLongPressItem(item)
+         }
+         >{item.title}</Text>
        </View>} />
      </View>
    </SafeAreaView>
@@ -48,7 +111,6 @@ function ToDoListScreen({navigation}: any)
 function ToDoScreen({route, navigation}: any)
 {
   const { title, todolist } = route.params;
-  console.log(title)
   const todo: ToDo = todolist.find((td: ToDo) => td.title == title);
 
   return <View>
@@ -58,11 +120,53 @@ function ToDoScreen({route, navigation}: any)
   </View>
 }
 
+function AddScreen({route, navigation}: any)
+{
+  const [date, setDate] = useState<Date>(new Date());
+  const [title, setTitle] = useState<string>('');
+  const [text, setText] = useState<string>('');
+
+  const item: ToDo = {
+    title: title,
+    text: text,
+    date: date,
+    state: false
+  }
+
+  return <View>
+    <Text>Title</Text>
+    <TextInput style={styles.input} onChange={(e) => setTitle(e.nativeEvent.text)}></TextInput>
+
+    <Text>Text</Text>
+    <TextInput style={styles.input} value={item.text} onChange={(e) => setText(e.nativeEvent.text)}></TextInput>
+
+    <Text>Date</Text>
+    <Text onPress={() =>
+    {
+      DateTimePickerAndroid.open({
+        value: date,
+        mode: "date",
+        onChange: (event, date) => setDate(date || new Date())
+      })
+    }
+    }>{item.date.toLocaleDateString()}</Text>
+
+    <Button title='Add' onPress={() =>
+    {
+      navigation.navigate('ToDoList', {
+        newItem: item
+      });
+    }
+    } />
+  </View>
+}
+
 export default function App() {
   return <NavigationContainer>
     <Stack.Navigator>
       <Stack.Screen name='ToDoList' component={ToDoListScreen} />
       <Stack.Screen name='ToDo' component={ToDoScreen} />
+      <Stack.Screen name='AddItem' component={AddScreen} />
     </Stack.Navigator>
   </NavigationContainer>
 }
@@ -70,8 +174,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    margin: 8
   },
+  listTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  input: {
+    width: '100%'
+  }
 });
